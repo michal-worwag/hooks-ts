a7e715cd37de61f82b0adb037837d55fbb40336a
Michał Worwąg <michalwo@gmail.com>
2024-11-23 17:45:43 +0100

    feat: implement useSlugify function and remove utils

--- src/useSlugify/useSlugify.ts
+++ src/useSlugify/useSlugify.ts
@@ -1,11 +1,10 @@
-// src/useSlugify/useSlugify.ts
-function useSlugify(str) {
+export function useSlugify(str: string): string {
   const slug = str
     .toLowerCase()
     .replace(/[^a-z0-9 -]/g, '')
     .replace(/\s+/g, '-')
     .replace(/-+/g, '-')
     .replace(/^-+|-+$/g, '');
+
   return slug;
 }
-export { useSlugify };
