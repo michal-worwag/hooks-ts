ea6b5cd70ae0377e7155a8c0f99d6040abd7b842
Michał Worwąg <michalwo@gmail.com>
2024-12-08 13:03:44 +0100

    feat: add HookList component and update available hooks documentation

---
+++ apps/www/src/libs/hooks.ts
@@ -0,0 +1,30 @@
+export const hooks = [
+  {
+    name: 'useDebounce',
+    url: 'https://hooks-ts.com/docs/useDebounce',
+  },
+  {
+    name: 'useDarkMode',
+    url: 'https://hooks-ts.com/docs/useDarkMode',
+  },
+  {
+    name: 'useHover',
+    url: 'https://hooks-ts.com/docs/useHover',
+  },
+  {
+    name: 'useLocalStorage',
+    url: 'https://hooks-ts.com/docs/useLocalStorage',
+  },
+  {
+    name: 'useMediaQuery',
+    url: 'https://hooks-ts.com/docs/useMediaQuery',
+  },
+  {
+    name: 'useNetworkStatus',
+    url: 'https://hooks-ts.com/docs/useNetworkStatus',
+  },
+  {
+    name: 'useSlugify',
+    url: 'https://hooks-ts.com/docs/useSlugify',
+  },
+];
